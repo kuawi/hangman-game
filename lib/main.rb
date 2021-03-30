9c36a7a007d4b5bb37bbd40d6c78400c3f918e13
Jose L. Lozano <jllozano.mx@gmail.com>
2021-03-30 15:20:52 -0600

    Create separated namespaces for UI and game logic methods

--- lib/main.rb
+++ lib/main.rb
@@ -2,13 +2,38 @@
 
 DICTIONARY = '../src/dictionary.txt'
 
-def choose_random_word(dictionary)
-  dictionary = File.open(dictionary)
-  random_pos = rand(dictionary.size)
-  dictionary.pos = random_pos
-  string = dictionary.read(26).downcase
-  dictionary.close
-  string.match(/\n[a-z]*\n/)[0].strip
+# any mathod that includes puts gets or print will be considered a UI method and belongs to this module
+module UI
+  def main_menu(word)
+    puts 'Welcome'
+    puts word
+  end
 end
 
-puts choose_random_word(DICTIONARY)
+# game logic
+class Game
+  include UI
+
+  def initialize
+    @secret_word = choose_random_word(DICTIONARY)
+    @lives = 10
+  end
+
+  def play
+    main_menu(@secret_word)
+  end
+
+  private
+
+  def choose_random_word(dictionary)
+    dictionary = File.open(dictionary)
+    random_pos = rand(dictionary.size)
+    dictionary.pos = random_pos
+    string = dictionary.read(26).downcase
+    dictionary.close
+    string.match(/\n[a-z]*\n/)[0].strip
+  end
+end
+
+hangman = Game.new
+hangman.play
