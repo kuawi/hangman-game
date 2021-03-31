6983b07a00e2e6214ceec2d2aec02ea0650bd04c
Jose L. Lozano <jllozano.mx@gmail.com>
2021-03-31 13:39:07 -0600

    Add basic functionality to main screen

--- lib/main.rb
+++ lib/main.rb
@@ -4,9 +4,21 @@ DICTIONARY = '../src/dictionary.txt'
 
 # any mathod that includes puts gets or print will be considered a UI method and belongs to this module
 module UI
+  SCREEN_SIZE = 30
   def main_menu(word)
     puts 'Welcome'
-    puts word
+    puts word.join
+    puts '********'.center(SCREEN_SIZE)
+  end
+
+  def main_screen(bar, lives)
+    puts "#{lives} lives remaining".rjust(SCREEN_SIZE)
+    puts ''
+    show_progress_bar(bar)
+  end
+
+  def show_progress_bar(bar)
+    puts bar.join
   end
 end
 
@@ -15,12 +27,14 @@ class Game
   include UI
 
   def initialize
-    @secret_word = choose_random_word(DICTIONARY)
+    @secret_word_arr = choose_random_word(DICTIONARY).split('')
+    @progress_arr = Array.new(@secret_word_arr.size, '-')
     @lives = 10
   end
 
   def play
-    main_menu(@secret_word)
+    main_menu(@secret_word_arr)
+    main_screen(@progress_arr, @lives)
   end
 
   private
